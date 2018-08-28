a718ed11db523141e30972a7258ad22d501d7c42
kangbin <527469786@qq.com>
2018-08-28 11:08:44 +0800

    双层变单层

--- 5.0Launcher/Launcher3/src/com/android/launcher3/LauncherAppState.java
+++ 5.0Launcher/Launcher3/src/com/android/launcher3/LauncherAppState.java
@@ -277,8 +277,10 @@ public class LauncherAppState implements DeviceProfile.DeviceProfileCallbacks {
 
     public static boolean isDisableAllApps() {
         // Returns false on non-dogfood builds.
-        return getInstance().mBuildInfo.isDogfoodBuild() &&
-                Launcher.isPropertyEnabled(Launcher.DISABLE_ALL_APPS_PROPERTY);
+        //return getInstance().mBuildInfo.isDogfoodBuild() &&
+        //        Launcher.isPropertyEnabled(Launcher.DISABLE_ALL_APPS_PROPERTY);
+		//双层变单层
+		return true;
     }
 
     public static boolean isDogfoodBuild() {
